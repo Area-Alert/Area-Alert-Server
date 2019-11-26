b9e6dba619087db6701cee4d088da5b7d7d3470f
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-11-26 22:41:29 +0530

    send notification

--- app.py
+++ app.py
@@ -4,41 +4,13 @@ from firebase_admin import messaging
 from firebase_admin import firestore
 from time import sleep
 from math import sqrt
+import threading
 
 cred = credentials.Certificate('./areaAlert.json')
 default_app = firebase_admin.initialize_app(cred)
 DEFAULT_RADIUS = 0.005
 ONE_KM_APPROXIMATE = 0.008162097402023085
-#
-# tokens = [  # 'feZjFPoN2x4:APA91bEekCxZEF1jOzyzWuZ5WtDrm4bNL9wf4kxhSMrH-LZ1enEOIu0tVLW5I7cilGIpi0qQEh4nPUf50-_khDbJVSFYwVMvH5cE1vhy2T9OSSxgu9Zr8gGqI-DKeV-MYJh-Uclv4cW3',
-#     # 'f4zqyOqSwaU:APA91bE6fJ15W8uiQn1dMIkrNsBIZNDQwg9oFtHB7Xl0p2MCUVAMEAPJwB3F6gOUEIoEiadFOTB0Tg1Mgybgnz6eGAXDA35PGblRoT8K77OnLGPy2DsNZApbEyPqEQXW8zUAkLdwVNW3',
-#     # 'cWd3unXIEss:APA91bHfNim0_OvZKy-o_8qMPPpN8Mgle59antarfd1rc8c7KZOiGiSJZArhpVvCtIVqekBjE0SCiFlc4d4N4qdkA8Utofz9ZvQEcReBxyBRyNPpZLq7heXVudKtrC9DS906kzrBF7hI',
-#     # 'c7HcaJhM3Sg:APA91bHQoSV6CZLJKT9XmkQY-GxTC_DtJpQWMlKVVFYPVV0dltGuHef073bPUMrhsDpU4oS8HWI8wrUWc5QjoOcQluaIbK4dCXJ_IZCKQtbdcDe4I2t2drzPPpJI033lOSa2A6dtaGjh',
-#     'c34TtdsfCVI:APA91bEp9frwzdKh-vQSl7zZ-PHPaNpR1MxO9wsDuaJoOEfKr1jm4RB5J8Sr4BmUlq1nAiHbDZgTMmSYdZZUSRWYY65V0jXBqWTvCdo2_6Ibiu0h6reIEcdGCXT8BaLEYbXNWMqspGU0',
-#     'c34TtdsfCVI:APA91bEp9frwzdKh-vQSl7zZ-PHPaNpR1MxO9wsDuaJoOEfKr1jm4RB5J8Sr4BmUlq1nAiHbDZgTMmSYdZZUSRWYY65V0jXBqWTvCdo2_6Ibiu0h6reIEcdGCXT8BaLEYbXNWMqspGU0',
-#     'c2KCi7uJdMI:APA91bFbZYAj1klVKYW9qq_IQgiaUqaxBIguOcOSjUGT9mLDiLcBDcILWQsCvzc-DRxoOrOobd24SOXiH7MD0wA_H1nWRcqET7fmipi_GRnH_lyaAWppQT-jixgoqMFpOQWTn8jRqEGQ',
-#     'er4ScJyRrls:APA91bFgTvOL4LjANNvFBdmZPukEmTy1mu_WQEWp4I1h-kV8RL45ShDDb3FCaJdzSQ7cnNuwfmrvJEWiGhUQz-6xI8ubHYVvt7LXWV_5jPO2uaNBHxUTcDBpxgqQihaPj3gY3OkYE6VQ',
-#     'cNwV2c6NPlE:APA91bGLPDtLF1Pe59jgFB2MWGyRVEx7IB-HBYf4SgPAYxuHVjruhXGeBuTAc77BPX69aJJKTfm5KokNchfByunCdOUVwh4kiQ2c6YFOYLhThTXgjb_3lJx2nZr4K25VikdmcV4MXCLt',
-#     'ffQKzbpiyEE:APA91bH18u2aJXG7VQwRgMplw9XuscWsyUfBpRrluiQ2SRZwMw0q0qfK2zoYS425jxyIvD5UCvFRwp8ypI2LqyYtu8rTQE-02BhxEBkHfvxed32t_2qV1eyfnx-Gwrb7-tXvrUm4QXYV',
-#
-# ]
-#
-# for token in tokens:
-#     try:
-#         message = messaging.Message(
-#             data={
-#                 'title': "Flash Flood Warning",
-#                 'body': 'Flash floods expected near your area, click to see what you can do',
-#                 'priority': '5',
-#                 'tag': 'Test'
-#             },
-#             token=token
-#         )
-#
-#         response = messaging.send(message)
-#         print('Successfully sent message:', response)
-#     except Exception as e:
-#         pass
+DEFAULT_PRIORITY = '3'
 
 db = firestore.client()
 
@@ -48,9 +20,9 @@ def is_user_in_report(user_doc, report_doc):
     user_data = user_doc.to_dict()
 
     try:  # fix db inconsistencies
-        epicenter_lat, epicenter_lon = report_data["lat"], report_data["lon"]
-    except Exception:
         epicenter_lat, epicenter_lon = report_data["location"].latitude, report_data["location"].longitude
+    except Exception:
+        epicenter_lat, epicenter_lon = report_data["lat"], report_data["lon"]
     user_lat, user_lon = user_data["currentLocation"].latitude, user_data["currentLocation"].longitude
 
     # strength
@@ -77,6 +49,21 @@ def get_in_reports(user_doc):
 
 def send_notification(to, about):
     print(to.to_dict(), "==================", about.to_dict())
+    to = to.to_dict()
+    about = about.to_dict()
+    
+    notification = messaging.Message(
+        data={
+            'title': str(about["report_type"]),
+            'body': f'Report about {about["report_type"]} nearby',
+            'priority': str(about["priority"]) if "priority" in about.keys() else DEFAULT_PRIORITY,
+            'tag': str(about["tag"]) if "tag" in about.keys() else 'test'
+        },
+        token=to["token"]
+    )
+
+    response = messaging.send(notification)
+    print("Sent Notification", response)
 
 
 def users_listener(collection_snapshot, changed_users_docs, read_time):  # initial call gets everything, duh.
@@ -88,11 +75,12 @@ def users_listener(collection_snapshot, changed_users_docs, read_time):  # initi
 
             if len(in_reports) is not 0:
                 for report in in_reports:
-                    send_notification(to=changed_user_doc.document, about=report)
+                    threading.Thread(target=send_notification, args=(changed_user_doc.document, report)).start()
+                    # send_notification(to=changed_user_doc.document, about=report)
             else:
                 continue
         except Exception as e:
-            pass
+            print("user listener", e)
 
 
 db.collection('users').on_snapshot(users_listener)
